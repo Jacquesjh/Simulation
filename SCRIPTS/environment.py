4cd14bf563014ec3922361a916b1bcfe4021c73c
João Pedro Jacques Hoss <61204659+Jacquesjh@users.noreply.github.com>
2021-06-19 21:58:00 -0300

    Fixed some area issues. More methods

--- SCRIPTS/environment.py
+++ SCRIPTS/environment.py
@@ -8,6 +8,7 @@ Created on Wed Jun 16 18:18:17 2021
 import numpy as np
 import random
 from population import *
+from utils import *
 
 class CommercialRegion():    
     def __init__(self, distance, area, x, y):        
@@ -24,10 +25,14 @@ class House():
         self.color       = color
         self.distance    = distance
         self.wealth      = wealth_base
-        self.num_members = int(np.random.normal((2/wealth)*3.5/(1 + 200*pow(np.e, - pow(area, 2)/2000)) + 1, 1/wealth,  1))
+        self.num_members = int(np.random.normal(loc = (4 + 1/wealth_base), scale = 1/wealth_base))
+        if self.num_members < 1:
+            self.num_members = 1
         self.members     = {}
         
-    #def generate_members(self, wealth):
+    def generate_house_members(self):
+        for i in range(self.num_members):
+            self.members[i] = Person(self.wealth)
         
     def get_all_members(self):
         people = []
@@ -64,16 +69,23 @@ class House():
         return list_members
         
 class DomesticRegion():
-    def __init__(self, distance, area, x, y, scale):        
+    def __init__(self, distance, area, x, y, scale):
         self.type          = 'Domestic'
         self.color         = 'green'
         self.distance      = distance
-        self.area          = area
+        if area > pow(np.pi*pow(scale, 1/2), 2):
+            self.area = pow(np.pi*pow(scale, 1/2), 2)/2
+        else:
+            self.area          = area
         self.x             = x
         self.y             = y
         self.wealth        = 0.8/(1 + distance*pow(np.e, - scale/area)) + 0.2
-        self.num_buildings = int(np.random.normal(area*pow(scale, 1/4), distance*pow(scale, 1/2), 1))
+        self.num_buildings = int(abs(np.random.normal(loc = (pow(self.area/np.pi, 1/2)/self.wealth),
+                                                      scale = distance*pow(scale, 1/2)/self.wealth)))
         self.buildings     = {}
+        for i in range(self.num_buildings):
+            building          = House(distance = self.distance, color = self.color, wealth_base = self.wealth, area = self.area)
+            self.buildings[i] = building
         
         
     def get_wealth(self):
@@ -87,11 +99,6 @@ class DomesticRegion():
     
     def get_distance(self):
         return self.distance
-    
-    def create_buildings(self):
-        for i in range(self.num_buildings):
-            building = House(self.distance, self.color, self.wealth, self.area)
-            self.buildings[i] = buildings
     
     def get_region_population(self):
         region_population = []
@@ -120,6 +127,13 @@ class DomesticRegion():
                 
         return list_members
     
+    def generate_region_population(self):
+        for building in self.buildings:
+            building.generate_house_members()
+            
+    def get_buildings(self):
+        return self.buildings
+            
 class IndustrialRegion():
     def __init__(self, distance, area, x, y):        
         self.type     = 'Industrial'
